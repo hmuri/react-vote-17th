7e0a7bd7557f83ba5344723e71be12aa76302252
hmuri <113418916+hmuri@users.noreply.github.com>
2023-06-27 12:53:43 +0900

    [fix] 브랜치 정리용

--- react-vote/src/page/VoteDemo.tsx
+++ react-vote/src/page/VoteDemo.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+
+interface Team{
+    name: string;
+    count: number;
+}
 
 function VoteDemo(){
     return(
